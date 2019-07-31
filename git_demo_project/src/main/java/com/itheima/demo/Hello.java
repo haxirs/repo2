79d2f979683d46be291712f0cfce6b6db21e8fc8
zhangsan <zhangsan@qq.com>
2019-07-31 19:42:55 +0800

    2019-7-31 提交

--- git_demo_project/src/main/java/com/itheima/demo/Hello.java
+++ git_demo_project/src/main/java/com/itheima/demo/Hello.java
@@ -1,7 +1,10 @@
 package com.itheima.demo;
 
 public class Hello {
+
     public static void main(String[] args) {
+
         System.out.println("hello git projet");
+        System.out.println("hello,123");
     }
 }
